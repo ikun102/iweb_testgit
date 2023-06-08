4358182ff4ca5c44d34efbd2e4cee923649c4035
MA <2267469413@qq.com>
2023-06-08 19:45:32 +0800

    这是一个提交信息

---
+++ src/main/java/com/iweb/pojo/Student.java
@@ -0,0 +1,17 @@
+package com.iweb.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author MA
+ * @date 2023/6/8 16:09
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private int no;
+    private String name;
+}
