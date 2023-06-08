8786680c6f28cab0e72ed10f64edcc47ea404229
MA <2267469413@qq.com>
2023-06-08 19:56:47 +0800

    这是一个提交信息2

---
+++ src/main/java/com/iweb/test1/HelloWord.java
@@ -0,0 +1,11 @@
+package com.iweb.test1;
+
+/**
+ * @author MA
+ * @date 2023/6/8 19:55
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
